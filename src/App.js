8dab3a2670a3cc95a997c7d64317bc1105e8d58c
IzzyAIR <izzycode2105@gmail.com>
2022-10-04 23:28:55 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './index.scss';
+import React from 'react';
+
+function App() {
+    // let count = 0;
+    let [count, setCount] = React.useState(0)
+    const onPlus = () => { 
+        setCount(count + 1); 
+    }; 
+    
+    const onMinus = () => { 
+        setCount(count - 1); 
+    }; 
+    const onReset = () => { 
+        setCount(count = 0); 
+    }; 
+
+    return (
+        <div className="App">
+            <div>
+                <h2>Счетчик:</h2>
+                <h1>{count}</h1>
+                <button className="minus" onClick={onMinus}>- Минус</button>
+                <button className="plus" onClick={onPlus}>Плюс +</button>
+                <button className="reset" onClick={onReset}>Обнулить!!</button>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
